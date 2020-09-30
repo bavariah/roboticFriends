b46b882b6ca3950324dfa1211e6af0da1ee18225
bavariah <48217024+bavariah@users.noreply.github.com>
2020-09-30 10:44:35 +0200

    Updates

---
+++ precache-manifest.c8e1c29132ac1f51ba042669600261f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd15a80250a48ede96ec",
+    "url": "/roboticFriends/static/css/main.1dbf973f.chunk.css"
+  },
+  {
+    "revision": "cd15a80250a48ede96ec",
+    "url": "/roboticFriends/static/js/main.8bf0f34d.chunk.js"
+  },
+  {
+    "revision": "5102eedddeb9ca7546cd",
+    "url": "/roboticFriends/static/js/runtime~main.4885f268.js"
+  },
+  {
+    "revision": "e648a2d2e9ae03e78300",
+    "url": "/roboticFriends/static/css/2.35bdbbbc.chunk.css"
+  },
+  {
+    "revision": "e648a2d2e9ae03e78300",
+    "url": "/roboticFriends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "052c4b3a50c0d30fa1fc9d618e0d236f",
+    "url": "/roboticFriends/index.html"
+  }
+];
