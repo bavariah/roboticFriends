e87b233a208c517592424c3a0f1b0f71057fe8d7
bavariah <48217024+bavariah@users.noreply.github.com>
2020-09-30 10:27:20 +0200

    files

---
+++ src/robots.js
@@ -0,0 +1,92 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    {
+      id: 11,
+      name: 'Mark Todo',
+      username: 'Mark',
+      email: 'mark@april.biz'
+    },
+    {
+      id: 12,
+      name: 'Yesss y',
+      username: 'Why yes',
+      email: 'yess@april.biz'
+    },
+    {
+      id: 13,
+      name: 'Wow Wow',
+      username: 'wowow',
+      email: 'wow@april.biz'
+    },
+    {
+      id: 14,
+      name: 'Brox',
+      username: 'Brox',
+      email: 'moto@april.biz'
+    },
+    {
+      id: 15,
+      name: 'Lolol',
+      username: 'lol',
+      email: 'lolo@april.biz'
+    },
+  ];
